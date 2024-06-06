4defa50e4f542b6754b29c7d1daa1b1610148776
Woongwoong <zer0__@naver.com>
2024-06-06 13:58:24 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const Photo = require('../models/photo');
+const User = require('../models/user');
 
 // 파일 업로드 설정
 const storage = multer.diskStorage({
@@ -15,26 +16,25 @@ const upload = multer({ storage: storage });
 
 // 홈 라우트
 router.get('/', async (req, res) => {
-    const photos = await Photo.find().sort({ uploadDate: -1 });
+    const photos = await Photo.find().populate('uploadedBy').sort({ uploadDate: -1 });
     res.render('home', { 
         title: 'Home',
         photos,
-        name: req.user ? req.user.name : 'Guest',
-        email: req.user ? req.user.email : 'guest@example.com'
+        name: req.session.user ? req.session.user.name : 'Guest',
+        email: req.session.user ? req.session.user.email : 'guest@example.com'
     });
 });
 
 // 사진 업로드 라우트
 router.post('/upload', upload.single('photo'), (req, res) => {
     const newPhoto = new Photo({
-        filename: req.file.filename
+        filename: req.file.filename,
+        uploadedBy: req.session.user._id
     });
     newPhoto.save()
         .then(() => res.redirect('/'))
         .catch(err => res.status(500).send(err));
 });
-
-// 기존 코드들
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
@@ -43,7 +43,21 @@ router.get('/home', function(req, res, next) {
 
 /* GET profile page. */
 router.get('/profile', function(req, res, next) {
-  res.render('profile', { title: 'Profile', name: req.user.name, email: req.user.email });
+  if (req.session.user) {
+    res.render('profile', { title: 'Profile', name: req.session.user.name, email: req.session.user.email });
+  } else {
+    res.redirect('/');
+  }
+});
+
+/* GET logout. */
+router.get('/logout', function(req, res, next) {
+  req.session.destroy(function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
