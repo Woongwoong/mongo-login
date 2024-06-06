c290b5c9ff14868c779301d54a48ae2e5d371c62
Woongwoong <zer0__@naver.com>
2024-06-06 22:38:53 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,58 +1,30 @@
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
 const session = require('express-session');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const photosRouter = require('./routes/photos');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
-// MongoDB 연결 설정
-mongoose.connect('mongodb://localhost:27017/photo_gallery', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.log(err));
+// MongoDB 연결
+mongoose.connect('mongodb://localhost:27017/mongo-login', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// 뷰 엔진 설정
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// 세션 설정
 app.use(session({
   secret: 'your_secret_key',
   resave: false,
   saveUninitialized: true
 }));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// 라우트 설정
+const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/photos', photosRouter);
 
-// 404 에러 핸들링
-app.use(function(req, res, next) {
-  next(createError(404));
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:8000');
 });
-
-// 에러 핸들링
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-module.exports = app;
