64c67b5208054c5d20818b452f42b8393f637810
Woongwoong <zer0__@naver.com>
2024-06-06 23:00:50 +0900

    Update Image.js

--- models/Image.js
+++ models/Image.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const imageSchema = new mongoose.Schema({
   url: String,
-  title: String
+  title: String,
+  uploadedBy: String // 업로드한 사용자 이름 저장
 });
 
 module.exports = mongoose.model('Image', imageSchema);
