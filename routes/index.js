0acb2f12acbc7bce1de250bc395118239299069c
Woongwoong <zer0__@naver.com>
2024-06-06 22:02:30 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -18,53 +18,52 @@ const upload = multer({ storage: storage });
 
 // 홈 라우트
 router.get('/', async (req, res) => {
-  const photos = await Photo.find().populate('uploadedBy').sort({ uploadDate: -1 });
-  res.render('home', { 
-    title: 'Home',
-    photos,
-    name: req.session.user ? req.session.user.name : 'Guest',
-    email: req.session.user ? req.session.user.email : 'guest@example.com'
-  });
+    const photos = await Photo.find().populate('uploadedBy').sort({ uploadDate: -1 });
+    res.render('home', { 
+        title: 'Home',
+        photos,
+        user: req.session.user || { name: 'Guest', email: 'guest@example.com' }  // 수정된 부분
+    });
 });
 
 // 사진 업로드 라우트
 router.post('/upload', upload.single('photo'), (req, res) => {
-  if (!req.session.user) {
-    return res.status(401).send('You must be logged in to upload photos');
-  }
+    if (!req.session.user) {
+        return res.status(401).send('You must be logged in to upload photos');
+    }
 
-  const newPhoto = new Photo({
-    filename: req.file.filename,
-    uploadedBy: req.session.user._id
-  });
+    const newPhoto = new Photo({
+        filename: req.file.filename,
+        uploadedBy: req.session.user._id
+    });
 
-  newPhoto.save()
-    .then(() => res.redirect('/'))
-    .catch(err => res.status(500).send(err));
+    newPhoto.save()
+        .then(() => res.redirect('/'))
+        .catch(err => res.status(500).send(err));
 });
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('home', { title: 'Home' });
+    res.render('home', { title: 'Home', user: req.session.user || { name: 'Guest', email: 'guest@example.com' } });  // 수정된 부분
 });
 
 /* GET profile page. */
 router.get('/profile', function(req, res, next) {
-  if (req.session.user) {
-    res.render('profile', { title: 'Profile', name: req.session.user.name, email: req.session.user.email });
-  } else {
-    res.redirect('/');
-  }
+    if (req.session.user) {
+        res.render('profile', { title: 'Profile', name: req.session.user.name, email: req.session.user.email });
+    } else {
+        res.redirect('/');
+    }
 });
 
 /* GET logout. */
 router.get('/logout', function(req, res, next) {
-  req.session.destroy(function(err) {
-    if (err) {
-      return next(err);
-    }
-    res.redirect('/');
-  });
+    req.session.destroy(function(err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
