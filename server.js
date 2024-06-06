d8d440ff1a76dfc045a9cd8fc2f0be8f5ea1e7a5
Woongwoong <zer0__@naver.com>
2024-06-06 13:50:38 +0900

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
