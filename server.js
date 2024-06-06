c626d1f3b423a390dcced1dc8e9014920bf07667
Woongwoong <zer0__@naver.com>
2024-06-06 23:24:28 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const multer = require('multer');  // multer 추가
 
 const app = express();
 
@@ -21,10 +22,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// multer 설정
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+const upload = multer({ storage: storage });
+
 // 라우트 설정
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index')(upload);  // upload 미들웨어를 라우트에 전달
 app.use('/', indexRouter);
 
-app.listen(8000, () => {
-  console.log('Server is running on http://localhost:8000');
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
 });
