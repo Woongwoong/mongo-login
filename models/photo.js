3aa6faced0b00b377fecbfdced1a4bf98c1bd4dd
Woongwoong <zer0__@naver.com>
2024-06-06 13:26:13 +0900

    Update photo.js

--- models/photo.js
+++ models/photo.js
@@ -1,8 +1,16 @@
+// models/photo.js
 const mongoose = require('mongoose');
 
 const photoSchema = new mongoose.Schema({
-    filename: String,
-    uploadDate: { type: Date, default: Date.now }
+  filename: String,
+  uploadedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  uploadDate: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Photo = mongoose.model('Photo', photoSchema);
