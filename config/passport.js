1d24b3d03c9d33c2f22c73fb726e899fb9fd6024
Woongwoong <zer0__@naver.com>
2024-06-07 14:52:50 +0900

    passport.js 업데이트

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,30 @@
-const jsdd
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user'); // 사용자 모델
+
+module.exports = function(passport) {
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => {
+            done(err, user);
+        });
+    });
+
+    passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password'
+    }, (email, password, done) => {
+        User.findOne({ email: email }, (err, user) => {
+            if (err) return done(err);
+            if (!user) {
+                return done(null, false, { message: 'Incorrect email.' });
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user);
+        });
+    }));
+};
