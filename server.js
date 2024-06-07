7aada897d6ca4c7d64a7bb84c75530a849433b33
Woongwoong <zer0__@naver.com>
2024-06-07 14:48:15 +0900

    server.js 업데이트

--- server.js
+++ server.js
@@ -1,43 +1,40 @@
 const express = require('express');
 const session = require('express-session');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
 const path = require('path');
-const multer = require('multer');  // multer 추가
+const indexRouter = require('./routes/index');
 
 const app = express();
 
-// MongoDB 연결
-mongoose.connect('mongodb://localhost:27017/mongo-login', { useNewUrlParser: true, useUnifiedTopology: true });
+require('./config/passport')(passport);
 
-// 세션 설정
-app.use(session({
-  secret: 'your_secret_key',
-  resave: false,
-  saveUninitialized: true
-}));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));  // 업로드 폴더를 static으로 설정
-app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// multer 설정
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
-});
-const upload = multer({ storage: storage });
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
-// 라우트 설정
-const indexRouter = require('./routes/index')(upload);  // upload 미들웨어를 라우트에 전달
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', indexRouter);
 
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
 app.listen(8000, () => {
-  console.log('Server is running on http://localhost:8000');
+    console.log('Server is running on port 8000');
 });
+
+module.exports = app;
